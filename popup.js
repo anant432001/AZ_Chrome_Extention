e0f9c69bba61a7ec51abbe60dba2237feb5bac68
Anant Pandey <anant432001@gmail.com>
2023-05-20 23:54:48 +0530

    Second Commit

--- popup.js
+++ popup.js
@@ -63,9 +63,9 @@ const onDelete = async (e) => {
     "bookmark-" + bookmarkUrl.toString().split("-").at(-1)
   );
   bookmarkElementToDelete.parentNode.removeChild(bookmarkElementToDelete);
+  // document.getElementsByClassName("list-inline text_white text-md-right")[0].lastChild.remove();
   await removeFromMemory(bookmarkUrl);
   
-  document.getElementsByClassName("list-inline text_white text-md-right")[0].lastChild.remove();
   // addBookMarkButton();
 };
 
